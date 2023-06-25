94c8819a805daaab3bc61f28337230d2bd15c474
prajwal <prajwalshah912@gmail.com>
2023-06-25 19:38:14 +0530

    added images and one sound

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 //if we use the handleClick() inside the eventlisterner then it will be the straight up function call so thats why we dont use ()
 let noOfDrums = document.querySelectorAll(".drum").length;
 
-for(let i=0; i<noOfDrums; i++ ){
 
+for(let i=0; i<noOfDrums; i++ ){
+    
     document.querySelectorAll(".drum")[i].addEventListener("click", () => {
-        alert("i got clicked")
+      var audio = new Audio('sounds/tom-1.mp3');
+      audio.play();
     })
 
 }
