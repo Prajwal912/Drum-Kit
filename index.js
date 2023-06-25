356ef1c64c988c3befe0694bfccf622100245224
prajwal <prajwalshah912@gmail.com>
2023-06-25 19:22:54 +0530

    looped clicked on buttons

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 //if we use the handleClick() inside the eventlisterner then it will be the straight up function call so thats why we dont use ()
-document.querySelector("button").addEventListener("click", () => {
-    alert("i got clicked")
-})
+let noOfDrums = document.querySelectorAll(".drum").length;
 
+for(let i=0; i<noOfDrums; i++ ){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", () => {
+        alert("i got clicked")
+    })
+
+}
+
+
+
+
+ 
