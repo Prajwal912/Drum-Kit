2251fce90a21641ea0074ad5d8d672b9aac7cc76
prajwal1221 <prajwalshah912@gmail.com>
2023-06-27 11:20:58 +0530

    audio removed

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ for(let i=0; i<noOfDrums; i++ ){
     
     document.querySelectorAll(".drum")[i].addEventListener("click", () => {
       var audio = new Audio('sounds/tom-1.mp3');
-      audio.play();
+      // audio.play();
     })
 
 }
